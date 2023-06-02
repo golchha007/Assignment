fe32143c5154d1a735670f3ba4659b3b485fdaba
GG <golchha007@gmail.com>
2023-06-02 17:17:11 +0530

    Removed unused code

--- src/main/java/com/assignment/backend/exception/DuplicateEmployeeException.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.backend.exception;
-
-public class DuplicateEmployeeException extends RuntimeException{
-
-    public DuplicateEmployeeException(String message){
-        super (message);
-    }
-}
--- src/main/java/com/assignment/backend/exception/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.assignment.backend.exception;
-
-public class EmployeeNotFoundException extends RuntimeException{
-    public EmployeeNotFoundException(int id) {
-        super("Could not find employee " + id);
-    }
-}
